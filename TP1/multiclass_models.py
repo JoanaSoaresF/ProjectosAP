f8dd6fcf26d9fc8128ed1f427d3ec6b650bc0518
JoanaSoaresF <js.faria@campus.fct.unl.pt>
2022-04-18 21:45:26 +0100

    Multiclass classification - modelo concluído

---
+++ TP1/multiclass_models.py
@@ -0,0 +1,128 @@
+# ##############################################################################
+#  Aprendizagem Profunda, TP1 2021/2022
+#  :Authors:
+#  Gonçalo Martins Lourenço nº55780
+#  Joana Soares Faria nº55754
+# ##############################################################################
+from datetime import datetime
+
+from keras import Input, Model
+from keras.callbacks import TensorBoard
+from keras.layers import Conv2D, Activation, BatchNormalization, MaxPooling2D, Flatten, Dense, Dropout, Concatenate
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import Adam, SGD
+
+now = datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S")
+root_logdir = "logs"
+log_dir = "{}/Multiclass-{}/".format(root_logdir, now)
+
+
+def convolution_stack_layer(inputs, filters, kernel_size, polling=True):
+    """
+    Creates a convolutional stack with two convolution layers, padding "same", ReLU activation and batch normalization
+    Max pooling of size 2 × 2 and same stride
+    Args:
+        polling: boolean to indicate if it uses polling in the end of the block
+        inputs: the inputs of the layer
+        filters: number of filters
+        kernel_size: kernel size
+
+    Returns: the last layer created
+
+    """
+    layer = Conv2D(filters, kernel_size, padding="same")(inputs)
+    layer = Activation("relu")(layer)
+    # Use batch normalization after activation so that input of following layer is standardized
+    layer = BatchNormalization()(layer)
+
+    layer = Conv2D(filters, kernel_size, padding="same")(layer)
+    layer = Activation("relu")(layer)
+    layer = BatchNormalization()(layer)
+
+    if polling:
+        layer = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(layer)
+
+    return layer
+
+
+def create_multiclass_model(input_shape):
+    inputs = Input(shape=input_shape, name='inputs')
+
+    # convolutional layers stacked
+    convolutional_layer = convolution_stack_layer(inputs, 64, (3, 3))
+    convolutional_layer = convolution_stack_layer(convolutional_layer, 32, (3, 3))
+    convolutional_layer = convolution_stack_layer(convolutional_layer, 16, (3, 3))
+
+    # Flatten before dense layers
+    layer = Flatten()(convolutional_layer)
+
+    # Dense layers
+    # TODO fazer sem dropoff para fazer imagem a comparar
+    layer = dense_block(layer, 256)
+    layer = dense_block(layer, 128)
+    layer = dense_block(layer, 64)
+
+    # output
+    layer = Dense(10)(layer)
+    output = Activation("softmax")(layer)
+
+    return Model(inputs, output)
+
+
+def dense_block(inputs, n, dropout=True):
+    layer = Dense(n)(inputs)
+    layer = Activation("relu")(layer)
+    # Use batch normalization after activation so that input of following layer is standardized
+    layer = BatchNormalization()(layer)
+    if dropout:
+        layer = Dropout(0.5)(layer)
+        # Use dropout for dense layers
+    return layer
+
+
+def train_multiclass_model(X, Y):
+    trainX, valX, trainY, valY = train_test_split(X, Y, train_size=3500, test_size=500)
+    tb_callback = TensorBoard(log_dir=log_dir, write_graph=True, write_images=True, profile_batch=0)
+    model = create_multiclass_model((64, 64, 3))
+
+    opt = Adam()
+
+    model.compile(loss="categorical_crossentropy",
+                  optimizer=opt,
+                  metrics=["accuracy"])
+    model.summary()
+    model.fit(trainX, trainY,
+              validation_data=(valX, valY),
+              batch_size=32,
+              epochs=100,
+              callbacks=[tb_callback])
+
+    model.save_weights('multiclass_best_model.h5')
+
+    # model.load_weights('multiclass.h5')
+
+    return model
+
+
+# unused model
+def model_with_residual_block(input_shape):
+    inputs = Input(shape=input_shape, name='inputs')
+    c1 = Conv2D(filters=64, kernel_size=(3, 3), activation="relu", padding="same")(inputs)
+    c1 = Conv2D(filters=64, kernel_size=(3, 3), padding="same")(c1)
+    c1 = Concatenate()([c1, inputs])
+    c1 = Activation("relu")(c1)
+    layer = MaxPooling2D(pool_size=(2, 2))(c1)
+
+    # Flatten before dense layers
+    layer = Flatten()(layer)
+
+    # Dense layers
+    layer = dense_block(layer, 256)
+    layer = dense_block(layer, 128)
+    layer = dense_block(layer, 64)
+
+    # output
+    layer = Dense(10)(layer)
+    output = Activation("softmax")(layer)
+
+    return Model(inputs, output)
--- TP1/tp1.py
+++ TP1/tp1.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Aprendizagem Profunda, TP1
-"""
+
+# ##############################################################################
+#  Aprendizagem Profunda, TP1 2021/2022
+#  :Authors:
+#  Gonçalo Martins Lourenço nº55780
+#  Joana Soares Faria nº55754
+# ##############################################################################
+from keras.callbacks import TensorBoard
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.optimizers import Adam
+
+from multiclass_models import train_multiclass_model
+import tp1_utils as util
+
+
+# tensorboard --logdir logs
+# http://127.0.0.1:6006
+
+def normalization(X):
+    X = X.reshape((X.shape[0], 64, 64, 3))
+    X = X.astype("float32") / 255.0
+    return X
+
+
+def main():
+    data = util.load_data()
+    train_X = normalization(data['train_X'])
+    train_classes = data['train_classes']
+    testX = normalization(data['test_X'])
+    test_classes = data['test_classes']
+    model = train_multiclass_model(train_X, train_classes)
+    multiclass_eval = model.evaluate(testX, test_classes)
+    print(f'Multiclass accuracy: {multiclass_eval[1]}; Multiclass loss: {multiclass_eval[0]}')
+    print(multiclass_eval)
+    """
+    Multiclass accuracy: 0.9879999756813049; Multiclass loss: 0.035707972943782806
+    [0.035707972943782806, 0.9879999756813049]
+    """
+
+
+main()
